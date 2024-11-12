8683e3870faa041f6220f7e6173419f014bf5c90
KOSASIH <kosasihg88@gmail.com>
2024-11-12 10:19:20 +0700

    Create test_setup.js

---
+++ tests/test_setup.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+beforeAll(async () => {
+  // Any global setup can be done here
+});
+
+afterAll(async () => {
+  // Any global teardown can be done here
+});
